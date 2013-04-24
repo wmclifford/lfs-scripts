adfc34717d4596ab448edf4a2a55b70e399d49da
William M. Clifford <wclifford@hosemccann.com>
2013-04-24 14:49:43 -0400

    * bin/cross-compile-stage.sh   Filled in the rest of the stage steps, sourcing in each package in turn,   and calling the cross_compile function for each one. Note that GCC   actually gets compiled twice during this stage - once statically, once   normally. To get the static cross compile to happen, another set of   functions will need to be added to the GCC package script, with the   master function named cross_compile_static.

--- bin/cross-compile-stage.sh
+++ bin/cross-compile-stage.sh
@@ -8,7 +8,19 @@ source ../lib/build-env.sh
 
 trap WAT EXIT
 
-source pkg-file.sh && cross_compile
+source pkg-linux-headers.sh ; cross_compile
+source pkg-file.sh ; cross_compile
+source pkg-m4.sh ; cross_compile
+source pkg-ncurses.sh ; cross_compile
+source pkg-gmp.sh ; cross_compile
+source pkg-mpfr.sh ; cross_compile
+source pkg-mpc.sh ; cross_compile
+source pkg-ppl.sh ; cross_compile
+source pkg-cloog.sh ; cross_compile
+source pkg-binutils.sh ; cross_compile
+source pkg-gcc.sh ; cross_compile_static
+source pkg-eglibc.sh ; cross_compile
+source pkg-gcc.sh ; cross_compile
 
 PKG_NAME="CLFS"
 PKG_VERS="2.0rc1"
