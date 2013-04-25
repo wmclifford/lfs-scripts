711d2f708309389eaccb41f964f28e099fc7f1f7
William M. Clifford <wclifford@hosemccann.com>
2013-04-25 16:31:44 -0400

    * lib/functions.sh   Added coloring to messages generated by the build process.   Fix: loop functions (fetch_all_patches, verify_all_patches) were not   incrementing the counter; was stuck in infinite loop.

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+COLOR_RED='\033[01;31m'
+COLOR_GREEN='\033[01;32m'
+COLOR_YELLOW='\033[01;33m'
+COLOR_BLUE='\033[01;34m'
+COLOR_MAGENTA='\033[01;35m'
+COLOR_CYAN='\033[01;36m'
+COLOR_WHITE='\033[01;37m'
+COLOR_RESET='\033[0m'
+
 function apply_patch_archive() {
 	local -i patch_index=${1}
 	local patch_file="$(basename ${PKG_PATCH_URI[$patch_index]})"
 	cd "${CLFS_SOURCES}/${PKG_SOURCE_DIR}"
-	echo "Applying patch '${PKG_PATCH_DESC[$patch_index]}' to source tree"
+	echo -e "${COLOR_YELLOW}Applying patch '${PKG_PATCH_DESC[$patch_index]}' to source tree${COLOR_RESET}"
 	tar xvf "${CLFS_SOURCES}/${patch_file}"
 }
 
@@ -12,12 +21,13 @@ function apply_patch_file() {
 	local -i patch_index=${1}
 	local patch_file="$(basename ${PKG_PATCH_URI[$patch_index]})"
 	cd "${CLFS_SOURCES}/${PKG_SOURCE_DIR}"
-	echo "Applying patch '${PKG_PATCH_DESC[$patch_index]}' to source tree"
+	echo -e "${COLOR_YELLOW}Applying patch '${PKG_PATCH_DESC[$patch_index]}' to source tree${COLOR_RESET}"
 	patch -Np1 -i "${CLFS_SOURCES}/${patch_file}"
 }
 
 function cleanup_package() {
 	set +e
+	echo -e "${COLOR_BLUE}Cleaning up package ${PKG_NAME}-${PKG_VERS}${COLOR_RESET}"
 	cd "${CLFS_SOURCES}"
 	if [ -d "${CLFS_SOURCES}/${PKG_SOURCE_DIR}" ] ; then
 		rm -rf ${CLFS_VERBOSE} "${CLFS_SOURCES}/${PKG_SOURCE_DIR}"
@@ -29,9 +39,10 @@ function cleanup_package() {
 }
 
 function dump_package() {
+	echo -e "${COLOR_BLUE}Dumping package ${PKG_ARCHIVE} ...${COLOR_RESET}"
 	cd "${CLFS_SOURCES}"
 	if [ ! -f "${CLFS_SOURCES}/${PKG_ARCHIVE}" ] ; then
-		echo "Missing: ${PKG_ARCHIVE}"
+		echo -e "${COLOR_RED}Missing: ${PKG_ARCHIVE}${COLOR_RESET}"
 		return 1
 	fi
 	tar ${CLFS_VERBOSE}xf ${PKG_ARCHIVE}
@@ -48,7 +59,9 @@ function fetch_all_patches() {
 	local -i patch_index=0
 	while [ $patch_index -lt $patch_count ] ; do
 		fetch_patch $patch_index
+		let patch_index=${patch_index}+1
 	done
+	/bin/true
 }
 
 function fetch_patch() {
@@ -58,15 +71,21 @@ function fetch_patch() {
 	local patch_filename="$(basename $patch_uri)"
 	cd "${CLFS_SOURCES}"
 	if [ ! -f "${CLFS_SOURCES}/${patch_filename}" ] ; then
-		echo "Retrieving patch \"$path_desc\" ..."
+		echo -e "${COLOR_CYAN}Retrieving patch \"$patch_desc\" ...${COLOR_RESET}"
 		wget "${PKG_PATCH_URI[$patch_index]}"
+	else
+		echo -e "${COLOR_WHITE}Patch \"$patch_desc\" already available.${COLOR_RESET}"
 	fi
+	/bin/true
 }
 
 function get_package() {
+	echo -e "${COLOR_CYAN}Retrieving package ${PKG_URI} ...${COLOR_RESET}"
 	cd "${CLFS_SOURCES}"
 	if [ ! -f "${CLFS_SOURCES}/${PKG_ARCHIVE}" ] ; then
 		wget "${PKG_URI}"
+	else
+		echo -e "${COLOR_WHITE}Package \"${PKG_ARCHIVE}\" already available.${COLOR_RESET}"
 	fi
 	fetch_all_patches
 }
@@ -84,7 +103,9 @@ function validate_all_patches() {
 	local -i patch_index=0
 	while [ $patch_index -lt $patch_count ] ; do
 		validate_patch $patch_index
+		let patch_index=${patch_index}+1
 	done
+	/bin/true
 }
 
 function validate_package() {
@@ -108,6 +129,7 @@ function GTG() {
 	if [ "${fn}" == "main" ] ; then
 		fn=${0}
 	fi
+	echo -e "${COLOR_GREEN}"
 	cat <<EOF
 
 ===============================================================
@@ -123,10 +145,11 @@ ${PKG_NAME}-${PKG_VERS}: ${fn}
 ===============================================================
 
 EOF
+	echo -e "${COLOR_RESET}"
 }
 
 function NTD() {
-	echo "${PKG_NAME}-${PKG_VERS}: ${FUNCNAME[1]} - nothing to do"
+	echo -e "${COLOR_MAGENTA}${PKG_NAME}-${PKG_VERS}: ${FUNCNAME[1]} - nothing to do${COLOR_RESET}"
 }
 
 function WAT() {
@@ -134,6 +157,7 @@ function WAT() {
 	if [ "${fn}" == "main" ] ; then
 		fn=${0}
 	fi
+	echo -e "${COLOR_RED}"
 	cat <<EOF
 
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -149,6 +173,7 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 EOF
-    exit 1
+	echo -e "${COLOR_RESET}"
+	exit 1
 }
 
