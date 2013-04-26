852637603b00803e23152f6c7d5834f533bb6b98
William M. Clifford <wclifford@hosemccann.com>
2013-04-26 08:14:24 -0400

    * bin/temporary-system-part1.sh   Fix: reset current path before sourcing in next package in list.

--- bin/temporary-system-part1.sh
+++ bin/temporary-system-part1.sh
@@ -13,37 +13,50 @@ declare -i CLFS_CHAPTER=6
 source ../lib/functions.sh
 source ../lib/build-env.sh
 
+# Make sure we are running as the CLFS user.
+if [ "$USER" != "${CLFS_USER}" ] ; then
+	echo -e "${COLOR_YELLOW}
+////////////////////////////////////////////////////////////////////////
+// WARNING: Presently running script as user '${USER}';
+//          This stage needs to be run as the CLFS user '${CLFS_USER}'.
+//          Aborting build.
+////////////////////////////////////////////////////////////////////////
+"
+	exit 1
+fi
+
 trap WAT EXIT
 
-source ../packages/pkg-gmp.sh ; temp_system
-source ../packages/pkg-mpfr.sh ; temp_system
-source ../packages/pkg-mpc.sh ; temp_system
-source ../packages/pkg-ppl.sh ; temp_system
-source ../packages/pkg-cloog.sh ; temp_system
-source ../packages/pkg-zlib.sh ; temp_system
-source ../packages/pkg-binutils.sh ; temp_system
-source ../packages/pkg-gcc.sh ; temp_system
-source ../packages/pkg-ncurses.sh ; temp_system
-source ../packages/pkg-bash.sh ; temp_system
-source ../packages/pkg-bison.sh ; temp_system
-source ../packages/pkg-bzip2.sh ; temp_system
-source ../packages/pkg-coreutils.sh ; temp_system
-source ../packages/pkg-diffutils.sh ; temp_system
-source ../packages/pkg-findutils.sh ; temp_system
-source ../packages/pkg-file.sh ; temp_system
-source ../packages/pkg-flex.sh ; temp_system
-source ../packages/pkg-gawk.sh ; temp_system
-source ../packages/pkg-gettext.sh ; temp_system
-source ../packages/pkg-grep.sh ; temp_system
-source ../packages/pkg-gzip.sh ; temp_system
-source ../packages/pkg-m4.sh ; temp_system
-source ../packages/pkg-make.sh ; temp_system
-source ../packages/pkg-patch.sh ; temp_system
-source ../packages/pkg-sed.sh ; temp_system
-source ../packages/pkg-tar.sh ; temp_system
-source ../packages/pkg-texinfo.sh ; temp_system
-source ../packages/pkg-vim.sh ; temp_system
-source ../packages/pkg-xz.sh ; temp_system
+CLFS_BUILD_ROOT="$(pwd)"
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-gmp.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-mpfr.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-mpc.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-ppl.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-cloog.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-zlib.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-binutils.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-gcc.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-ncurses.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-bash.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-bison.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-bzip2.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-coreutils.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-diffutils.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-findutils.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-file.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-flex.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-gawk.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-gettext.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-grep.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-gzip.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-m4.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-make.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-patch.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-sed.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-tar.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-texinfo.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-vim.sh ; temp_system
+cd "${CLFS_BUILD_ROOT}" ; source ../packages/pkg-xz.sh ; temp_system
 
 PKG_NAME="CLFS"
 PKG_VERS="2.0.rc1"
